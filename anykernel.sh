81166bf8952045bdd277b1d4a0cd19c884b678a5
saurabhpattebahadur7 <saurabhpattebahadur7@gmail.com>
2025-11-30 00:15:56 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Hydrogen Kernel by rio113 / z3rokwq
+kernel.string=Rolex Kernel by rio113 / z3rokwq / Rolex040623
 do.devicecheck=1
 do.modules=0
 do.systemless=1
